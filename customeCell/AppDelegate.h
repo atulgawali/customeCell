6df0c9ff7e40dbc2492aa818304d9dcd07008663
Atul Gawali <atulgawali@Atuls-iMac.local>
2015-06-07 02:11:54 -0700

    Initial Commit

---
+++ customeCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customeCell
+//
+//  Created by Atul Gawali on 07/06/15.
+//  Copyright (c) 2015 Atul Gawali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customeCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customeCell
+//
+//  Created by Atul Gawali on 07/06/15.
+//  Copyright (c) 2015 Atul Gawali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
